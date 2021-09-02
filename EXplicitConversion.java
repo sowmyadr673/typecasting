07fe4573f6e27edcc3d9443ccc3e835558c63960
sowmyadr673 <89777877+sowmyadr673@users.noreply.github.com>
2021-09-02 18:55:53 +0530

    Add files via upload

---
+++ EXplicitConversion.java
@@ -0,0 +1,9 @@
+class EXplicitConversion{
+  public static void main(String[] args) {
+    double myDouble = 9.7;
+    int myInt = (int) myDouble;
+
+    System.out.println(myDouble);  
+    System.out.println(myInt);      
+  }
+}
---
+++ ImplicitConvertion.java
@@ -0,0 +1,9 @@
+ class ImplicitConvertion {
+  public static void main(String[] args) {
+    int myInt = 9;
+    double myDouble = myInt; 
+
+    System.out.println(myInt);      
+    System.out.println(myDouble);   
+  }
+}
